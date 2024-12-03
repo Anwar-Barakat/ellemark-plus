f134684d6de06ce35524bb503b38b9cf7373e494
Anwar-Barakat <brkatanwar0@gmail.com>
2024-12-03 09:55:55 +0400

    feat: integrate Vuetify and update project structure
    - Added Vuetify as a UI framework with necessary components and directives.
    - Updated package.json and package-lock.json to include Vuetify and related dependencies.
    - Refactored App.vue to utilize Vuetify components.
    - Removed HelloWorld component and associated styles.
    - Created router setup for navigation.
    - Added HomeView component as a placeholder for future development.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import router from "./router";
+import { createPinia } from "pinia";
 
-createApp(App).mount('#app')
+// Vuetify
+import vuetify from '@/plugins/vuetify'
+
+createApp(App)
+.use(router)
+.use(createPinia())
+.use(vuetify).mount("#app")
